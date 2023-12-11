8f2edb26b9f3e864a14b905f68b7eb37a8a78a86
rijarandria <rijandrisolo@gmail.com>
2023-12-11 21:05:49 +0100

    commit jeu du pendu pas encore au point

---
+++ exo16_interactifJeuPendu.py
@@ -0,0 +1,46 @@
+erreur = 10
+f=open('mots.txt')
+tableau=[]
+tabmot=[]
+tabjuste=[]
+for line in f.readlines() :
+    tableau.append(line.strip())
+#mot=input("Saisir le mot à chercher :")
+nombre = len(tableau)
+chercher = int(input(f"Choisissez un chiffre de 1 à {nombre} pour avoir le mot que vous devez chercher : "))
+mot = (tableau[chercher])
+#print("le mot à chercher", mot)
+lettre = len(mot)
+for i in range (len(mot)) :
+    tabjuste.append("*")
+print(tabjuste)
+print("Le mot à chercher est de :", lettre, "lettres. Vous avez ", erreur, "essais")
+while erreur>0 :
+    motSaisi = input("Votre mot : ")
+    tabmot = []
+    for lettre in motSaisi :
+       for mo in mot :
+           if(lettre.upper() == mo.upper()) :
+              tabmot.append(lettre.upper())
+    tabmot = set(tabmot)
+    print("Vous avez trouvé ", len(tabmot), "lettre(s)")
+    #print("tabmot", tabmot)
+    #print("mot", mot)
+    for tabmo  in tabmot :
+        for i in range (len(mot)) :
+            if mot[i] == tabmo :
+                #print(i)
+                tabjuste[i] = tabmo
+                #print("test", tabjuste)
+
+    print(str(tabjuste).upper())
+    if(tabjuste.count('*') == 0) :
+        print("bravo, vous avez trouvé le mot")
+        print(mot)
+        break
+
+
+    erreur -=1
+    print("Vous avez encore : ", erreur, "essai(s)")
+    if(erreur == 0) :
+        print("vous avez perdu")
