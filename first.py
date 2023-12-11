900f2a0097e8e4c87e8a99aff330a282c38cad54
rijarandria <rijandrisolo@gmail.com>
2023-12-11 10:14:38 +0100

    avec correction

--- first.py
+++ first.py
@@ -1,15 +1,17 @@
 #boucle pair
 print("chiffres pair de 0 à 10")
+som = 0
 for i in range(10):
     if i%2 == 0:
-        print(i)
+        som+=i
+print("somme de nombres pairs", som)
+
 print("----------------------------")
 print("chiffres impair de 0 à 10")
 #boucle impair
 for i in range(10):
     if i%2 == 1:
         print(i)
-
 print("----------------------------")
 
 liste = [1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4]
--- tp05_fonctions.py
+++ tp05_fonctions.py
@@ -1,49 +1,50 @@
+# exo 1 addition
+def addition(a, b):
+    print("addition", a + b)
 
-#exo 1 addition
-def addition(a,b) :
-    print("addition", a+b)
-#exo_2 palindrome
+
+# exo_2 palindrome
 def is_palindrome(char):
-    response ="non"
-    for i in range(len(char)//2):
-       if char[i] == char[-i-1]:
+    response = "non"
+    for i in range(len(char) // 2):
+        if char[i] == char[-i - 1]:
             response = "oui"
-            print(char[i], char[-i-1])
+            print(char[i], char[-i - 1])
     print(response)
-#exo_3 entre les min et max
-def min_max(a,b, c) :
+    #[::-1]
+
+# exo_3 entre les min et max
+def min_max(a, b, c):
     r = range(a, b)
     print("fourchette", r)
     d = []
-    for i in c :
-        if i in range(a, b) :
+    for i in c:
+        if i in range(a, b):
             d.append(i)
             print(d)
-    print("les valeurs sont", c)
+    print("la liste est", c)
     print("Les valeurs entre ", a, "et", b, "sont", d)
 
-#exo_4 calcul moyenne
-def moyenne(a) :
 
+# exo_4 calcul moyenne
+def moyenne(a):
     b = len(a)
-    print(b)
+    print("La longueur de la liste est :", b)
     val = 0
-    for i in a :
+    for i in a:
         val += i
-        print("intérieur", val)
+        print("somme des valeurs", val)
     print("total", val)
-    val =val/b
+    val = val / b
     print("la moyenne", val)
+    #sum(liste/len(liste))
+
 print("------------tp05-------------")
 print("--------- addition---------")
-addition(4,5)
+addition(4, 5)
 print("--------- palindrome---------")
-is_palindrome("bab")
+is_palindrome("radare")
 print("--------- entre min et max---------")
 min_max(2, 8, [1, 5, 6, 0, 7])
 print("--------- moyenne d'une liste---------")
 moyenne([10, 5, 6, 5, 7])
-
-
-
-
