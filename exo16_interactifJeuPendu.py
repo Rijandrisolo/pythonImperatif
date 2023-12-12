bac24829503978825c71766ecb40695a6b16219a
rijarandria <rijandrisolo@gmail.com>
2023-12-12 10:14:20 +0100

    commit jeu du pendu ok

--- exo16_interactifJeuPendu.py
+++ exo16_interactifJeuPendu.py
@@ -1,13 +1,15 @@
-erreur = 10
-f=open('mots.txt')
+essai = 10
 #tableau pour mettre les mots venants de mots.txt
 tableau=[]
 #permet de boucler pour afficher dans le terminal les lettres trouvées
 tabmot=[]
-#affichage des mots trouvés
+#affichage des lettres trouvées
 tabjuste=[]
+#######################################################
+f=open('mots.txt')
 for line in f.readlines() :
     tableau.append(line.strip())
+f.close()
 #mot=input("Saisir le mot à chercher :")
 nombre = len(tableau)
 chercher = int(input(f"Choisissez un chiffre de 1 à {nombre} pour avoir le mot que vous devez chercher : "))
@@ -17,8 +19,8 @@ lettre = len(mot)
 for i in range (len(mot)) :
     tabjuste.append("*")
 print(tabjuste)
-print("Le mot à chercher est de :", lettre, "lettres. Vous avez ", erreur, "essais")
-while erreur>0 :
+print("Le mot à chercher est de :", lettre, "lettres. Vous avez ", essai, "essais")
+while essai>0 :
     motSaisi = input("Votre mot : ")
     tabmot = []
     for lettre in motSaisi :
@@ -27,7 +29,7 @@ while erreur>0 :
               tabmot.append(lettre.upper())
     tabmot = set(tabmot)
     print("Vous avez trouvé ", len(tabmot), "lettre(s)")
-    for tabmo  in tabmot :
+    for tabmo in tabmot :
         for i in range (len(mot)) :
             if mot[i] == tabmo :
                 tabjuste[i] = tabmo
@@ -39,9 +41,9 @@ while erreur>0 :
         print("\O/")
         print(mot)
         break
-    erreur -=1
-    if erreur > 0 :
-        print("Vous avez encore : ", erreur, "essai(s)")
-    if(erreur == 0) :
+    essai -=1
+    if essai > 0 :
+        print("Vous avez encore : ", essai, "essai(s)")
+    if(essai == 0) :
         print("vous avez perdu")
         print("le mot était", mot.upper())
